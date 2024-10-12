306a2f35f20c5df6e54a0a48bcfad579a3da83ea
Simone Manti <manti998@gmail.com>
2024-10-12 19:22:05 -0400

    Added cartpole results.

--- src/symbolic_mbrl/cartpole.py
+++ src/symbolic_mbrl/cartpole.py
@@ -8,6 +8,7 @@ import mbrl.util.common as common_util
 import mbrl.models as models
 import mbrl.planning as planning
 from symbolic_mbrl.pets import pets
+import matplotlib.pyplot as plt
 
 
 def main(method, device):
@@ -85,11 +86,77 @@ if __name__ == "__main__":
     all_mse_sr = np.zeros((10, num_runs))
     all_mse_nn = np.zeros((10, num_runs))
 
-    for i in range(num_runs):
-        all_reward_sr[:, i], all_mse_sr[:, i] = main("SR", device_sr)
-        all_reward_nn[:, i], all_mse_nn[:, i] = main("NN", device_nn)
+    # for i in range(num_runs):
+    #    all_reward_sr[:, i], all_mse_sr[:, i] = main("SR", device_sr)
+    #    all_reward_nn[:, i], all_mse_nn[:, i] = main("NN", device_nn)
 
-    np.save("cartpole_rewards_sr.npy", all_reward_sr)
-    np.save("cartpole_mse_sr.npy", all_mse_sr)
-    np.save("cartpole_rewards_nn.npy", all_reward_nn)
-    np.save("cartpole_mse_nn.npy", all_mse_nn)
+    # np.save("cartpole_rewards_sr.npy", all_reward_sr)
+    # np.save("cartpole_mse_sr.npy", all_mse_sr)
+    # np.save("cartpole_rewards_nn.npy", all_reward_nn)
+    # np.save("cartpole_mse_nn.npy", all_mse_nn)
+
+    num_training_data = 10*np.arange(1, 11)
+
+    all_reward_sr = np.load("cartpole_rewards_sr.npy")
+    all_reward_nn = np.load("cartpole_rewards_nn.npy")
+    all_mse_sr = np.load("cartpole_mse_sr.npy")
+    all_mse_nn = np.load("cartpole_mse_nn.npy")
+
+    # mean std reward across multiple runs
+    mean_reward_sr = np.mean(all_reward_sr, axis=1)
+    std_reward_sr = np.std(all_reward_sr, axis=1)
+    mean_reward_nn = np.mean(all_reward_nn, axis=1)
+    std_reward_nn = np.std(all_reward_nn, axis=1)
+
+    # mean std mse across multiple runs
+    mean_mse_sr = np.mean(all_mse_sr, axis=1)
+    std_mse_sr = np.std(all_mse_sr, axis=1)
+    mean_mse_nn = np.mean(all_mse_nn, axis=1)
+    std_mse_nn = np.std(all_mse_nn, axis=1)
+
+    # --- PLOTS ---
+    width = 443.57848
+    fontsize = 8
+    plt.rcParams['font.size'] = fontsize
+    plt.rcParams['font.sans-serif'] = 'Dejavu Sans'
+    plt.rcParams['font.family'] = 'sans-serif'
+
+    # Convert from pt to inches
+    inches_per_pt = 1 / 72.27
+    # Figure width in inches
+    fig_width_in = width * inches_per_pt
+
+    fig_dim = (fig_width_in, 3.)
+
+    fig, ax = plt.subplots(nrows=1, ncols=2, figsize=fig_dim)
+
+    ax[0].plot(num_training_data,
+               mean_reward_sr, c="#e41a1c", label="SR-PETS")
+    ax[0].fill_between(num_training_data, mean_reward_sr - std_reward_sr,
+                       mean_reward_sr + std_reward_sr, color="#e41a1c", alpha=0.5)
+    ax[0].plot(num_training_data,
+               mean_reward_nn, c="#4daf4a", label="NN-PETS")
+    ax[0].fill_between(num_training_data, mean_reward_nn - std_reward_nn,
+                       mean_reward_nn + std_reward_nn, color="#4daf4a", alpha=0.5)
+    ax[0].set_xlabel("# Training Data")
+    ax[0].set_ylabel("Reward")
+    ax[0].legend()
+    ax[0].set_xlim(10, 100)
+    ax[0].text(-0.3, 1., "A",
+               transform=ax[0].transAxes, size=fontsize, weight="bold")
+
+    ax[1].plot(num_training_data,
+               mean_mse_sr, c="#e41a1c", label="SR-PETS")
+    ax[1].plot(num_training_data,
+               mean_mse_nn, c="#4daf4a", label="NN-PETS")
+    ax[1].set_yscale("log")
+    ax[1].set_xlabel("# Training Data")
+    ax[1].set_ylabel("Validation MSE")
+    ax[1].legend()
+    ax[1].set_xlim(10, 100)
+    ax[1].text(-0.35, 1., "B",
+               transform=ax[1].transAxes, size=fontsize, weight="bold")
+
+    fig.tight_layout()
+
+    plt.savefig("cartpole.pdf", dpi=300)
